cb0b692cdacd53e4ec427ba7dbb651e2bb1bd3fa
Valerie Gibson <vgibson14@gmail.com>
2016-04-11 21:52:42 +0100

    additional assert

--- code/spec/playerSelection_spec.js
+++ code/spec/playerSelection_spec.js
@@ -49,7 +49,7 @@ describe("Player Selection", function() {
     expect(playerSelection.nextPlayerIndex()).toBe(1);
   });
 
-  it("should udate current player", function() {
+  it("should update current player", function() {
     playerSelection = new PlayerSelection(players);
     playerSelection.updateCurrentPlayer();
     expect(playerSelection.currentPlayer.name).toBe("Jay");
