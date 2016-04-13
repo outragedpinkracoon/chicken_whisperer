bc702eaf293f3ed4907cca30fdff0c6eeb908ad3
Valerie Gibson <vgibson14@gmail.com>
2016-04-13 14:07:18 +0100

    tidy up

--- code/turn.js
+++ code/turn.js
@@ -4,8 +4,8 @@ var Dice = require("./die.js")
 class Turn {
 
   constructor(player, chickenPen, diceCollection){
-    if (!player) throw new Error("Turn must have player");
-    if (!chickenPen) throw new Error("Turn must have chicken pen");
+    if (player === undefined) throw new Error("Turn must have player");
+    if (chickenPen === undefined) throw new Error("Turn must have chicken pen");
     
     this.player = player;
     this.chickenPen = chickenPen;
@@ -15,19 +15,22 @@ class Turn {
 
   approachChicken(){
     var result = this.approachRoll();
+    
     if(result.isEven()){
       this.diceCollection.addCaptureDie();
       return;
     }
-    
+    //scare chickens
   }
 
   approachRoll(){
     var result = 0;
+    var limit = this.diceCollection.approachDice;
 
-    for(var i = 0; i < this.diceCollection.approachDice; i++) {
+    for(var i = 0; i < limit; i++) {
       result += this.diceCollection.die.roll();
     }
+
     return result;
   }
 
