d203c7552052ddfd327310f47c4ac34b62696891
Valerie Gibson <vgibson14@gmail.com>
2016-04-14 08:01:02 +0100

    fixes specs with new data

--- code/spec/game_spec.js
+++ code/spec/game_spec.js
@@ -3,6 +3,7 @@ var Game = require('../game.js')
 var Player = require('../player.js')
 var Chicken = require('../chicken.js')
 var ChickenPen = require('../chickenPen.js')
+var ChickenData = require('./chickenData.js')
 
 describe("Game", function() {
 
@@ -15,40 +16,9 @@ describe("Game", function() {
 
     var players = [player1, player2];
 
-    var chicken1_options = {
-      name: "Old Chicken",
-      speed: 5,
-      maxScare: 3
-    }
+    var chickenData = new ChickenData();
 
-    var chicken2_options = {
-      name: "Young Chicken",
-      speed: 14,
-      maxScare: 1
-    }
-
-    var chicken3_options = {
-      name: "Anime Chicken",
-      speed: 10,
-      maxScare: 2
-    }
-
-    var chicken4_options = {
-      name: "Average Chicken",
-      speed: 8,
-      maxScare: 2
-    }
-
-    var chicken_1 = new Chicken(chicken1_options);
-    var chicken_2 = new Chicken(chicken2_options);
-    var chicken_3 = new Chicken(chicken3_options);
-    var chicken_4 = new Chicken(chicken4_options);
-
-    var chickens = [chicken_1, chicken_2, chicken_3, chicken_4]
-
-    var chickenPen = new ChickenPen(chickens);
-
-    game = new Game(players, chickenPen);
+    game = new Game(players, data.chickenPen);
 
   });
 
--- code/spec/turn_spec.js
+++ code/spec/turn_spec.js
@@ -3,55 +3,23 @@ var Player = require("../player.js")
 var Turn = require("../turn.js")
 var Chicken = require("../chicken.js")
 var ChickenPen = require("../chickenPen.js")
+var ChickenData = require("./chickenData.js")
 var Die = require("../die.js")
 var DiceCollection = require("../diceCollection.js")
 
 describe("Turn", function(){
   var player;
-  var chicken1;
-  var chickens;
-  var chickenPen;
+  var data;
   var turn;
 
   beforeEach(function(){
     player = new Player("Valerie");
 
-    var chickenOneOptions = {
-      name: "Old Chicken",
-      speed: 5,
-      maxScare: 3
-    }
-
-    var chickenTwoOptions = {
-      name: "Young Chicken",
-      speed: 14,
-      maxScare: 1
-    }
-
-    var chickenThreeOptions = {
-      name: "Anime Chicken",
-      speed: 10,
-      maxScare: 2
-    }
-
-    var chickenFourOptions = {
-      name: "Average Chicken",
-      speed: 8,
-      maxScare: 2
-    }
-
-    chicken1 = new Chicken(chickenOneOptions);
-    var chicken2 = new Chicken(chickenTwoOptions);
-    var chicken3 = new Chicken(chickenThreeOptions);
-    var chicken4 = new Chicken(chickenFourOptions);
-
-    chickens = [chicken1, chicken2, chicken3, chicken4]
-
-    chickenPen = new ChickenPen(chickens);
-
     var diceCollection = new DiceCollection(new Die());
 
-    turn = new Turn(player, chickenPen, diceCollection);
+    var data = new ChickenData();
+
+    turn = new Turn(player, data.chickenPen, diceCollection);
   })
 
   it("should have player", function(){
