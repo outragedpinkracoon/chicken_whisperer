9c4095651763645b76c552d3be840ce5859ebb6c
Valerie Gibson <vgibson14@gmail.com>
2016-04-18 21:46:20 +0100

    refactor

--- code/captureTurn.js
+++ code/captureTurn.js
@@ -1,12 +1,14 @@
 require("./extensions/number.js")
+
 var Approach = require('./approach.js');
 var Capture = require('./capture.js');
 
 class CaptureTurn {
 
   constructor(options){
+
     this.player = options.player;
-    this.approach = new Approach( {chickenPen: this.chickenPen} );
+    this.approach = new Approach( {chickenPen: options.chickenPen} );
     this.capture = new Capture();
 
     this.finished = false;
