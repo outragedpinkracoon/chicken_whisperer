2408831388fd766d31e9a30dc1bed9c471d4cd6e
Valerie Gibson <vgibson14@gmail.com>
2016-04-18 20:53:04 +0100

    breaks up methods

--- code/spec/capture_spec.js
+++ code/spec/capture_spec.js
@@ -34,17 +34,31 @@ describe("Capture", function(){
     expect(result).toBe(true);
   });
 
+  it("should be an unsuccessful roll", function(){
+    var result = capture.successfulRoll(1, data.chicken1);
+    expect(result).toBe(false);
+  });
+
   it("should reduce chicken's speed on failure", function(){
     spyOn(capture.die, "roll").and.returnValues(1,1);
-    var result = capture.attempt(player, data.chicken1, data.chickenPen, 1);
+    var result = capture.failure(data.chicken1);
     
     expect(result).toBe(false);
     expect(data.chicken1.speed).toBe(4);
   });
 
-  it("should not alter chicken speed on success", function(){
-    spyOn(capture.die, "roll").and.returnValues(10);
+
+  it("should invoke failure", function(){
+    spyOn(capture.die, "roll").and.returnValues(1,1);
+    spyOn(capture, "failure").and.returnValues(1,1);
+
     var result = capture.attempt(player, data.chicken1, data.chickenPen, 1);
+    
+    expect(capture.failure).toHaveBeenCalled();
+  });
+
+  it("should not alter chicken speed on success", function(){
+    var result = capture.success(data.chickenPen, player, data.chicken1);
 
     expect(result).toBe(true);
     expect(data.chicken1.speed).toBe(5);
@@ -52,7 +66,7 @@ describe("Capture", function(){
 
   it("should add chicken to player on success", function(){
     spyOn(capture.die, "roll").and.returnValues(10);
-    var result = capture.attempt(player, data.chicken1, data.chickenPen, 1);
+    var result = capture.success(data.chickenPen, player, data.chicken1);
 
     expect(result).toBe(true);
     expect(player.chickens[0]).toEqual(data.chicken1);
@@ -60,10 +74,18 @@ describe("Capture", function(){
 
   it("should remove chicken from chickenPen on success", function(){
     spyOn(capture.die, "roll").and.returnValues(10);
-    var result = capture.attempt(player, data.chicken1, data.chickenPen, 1);
+    var result = capture.success(data.chickenPen, player, data.chicken1);
 
     expect(result).toBe(true);
     expect(data.chickenPen.count()).toEqual(3);
+  });
+
+  it("should invoke success", function(){
+    spyOn(capture.die, "roll").and.returnValues(10);
+    spyOn(capture, "success");
+    var result = capture.attempt(player, data.chicken1, data.chickenPen, 1);
+
+    expect(capture.success).toHaveBeenCalled();
   });
 
 })
