0e6f96e6087481ede5d73bd2ba62f4334c057db0
Valerie Gibson <vgibson14@gmail.com>
2016-04-22 08:45:01 +0100

    refactors rollMultiple

--- code/die.js
+++ code/die.js
@@ -14,13 +14,19 @@ class Die {
       func = (x,y) => x + y;
     }
 
+    results = rollMultiple(times)
+    
+    
+    return results.reduce((prev,curr) => func(prev,curr));
+  }
+
+  rollMultiple(times){
     var results = [];
     for(var i = 0; i < times; i++)
     {
       results.push(this.roll());
     }
-    
-    return results.reduce((prev,curr) => func(prev,curr));
+    return results;
   }
 
 }
